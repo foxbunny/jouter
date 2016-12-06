62745fc132042e8e06327c43ebebda066bc465a6
Hajime Yamasaki Vukelic <hayavuk@gmail.com>
2016-12-06 18:32:11 +0100

    Rebuilt dists

--- dist/jouter.js
+++ dist/jouter.js
@@ -1,53 +1,76 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-'use strict';
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(['exports'], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(exports);
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(mod.exports);
+    global.jouter = mod.exports;
+  }
+})(this, function (exports) {
+  'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+  function _toConsumableArray(arr) {
+    if (Array.isArray(arr)) {
+      for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) {
+        arr2[i] = arr[i];
+      }
+
+      return arr2;
+    } else {
+      return Array.from(arr);
+    }
+  }
+
+  var TOKEN_RE = /:[^\/]+/g;
+  var ANY_RE = /\*/g;
+
+  // parseRoute :: String -> RegExp
+  var routeRe = exports.routeRe = function routeRe(x) {
+    return new RegExp('^' + x.replace(TOKEN_RE, '([^/]+)').replace(ANY_RE, '.*') + '$');
+  };
+
+  // route :: (* -> *), String -> Route -> (String -> _)
+  var route = exports.route = function route(f, r) {
+    var re = routeRe(r);
+    return function (path) {
+      var match = re.exec(path);
+      if (!match) return;
+      f.apply(undefined, _toConsumableArray(match.slice(1)));
+    };
+  };
+
+  var createRouter = exports.createRouter = function createRouter() {
+    var routes = [];
+
+    var add = function add(f, r) {
+      return routes.push(route(f, r));
+    };
+    var dispatch = function dispatch() {
+      return routes.forEach(function (x) {
+        return x(window.location.pathname);
+      });
+    };
+    var go = function go(path, title) {
+      window.history.pushState(undefined, title, path);
+      dispatch();
+    };
+    var handleEvent = function handleEvent(e) {
+      e.preventDefault();
+      go(e.target.href, e.target.title);
+    };
+    var start = function start() {
+      window.onpopstate = dispatch;
+      dispatch();
+    };
+
+    return { add: add, go: go, handleEvent: handleEvent, start: start };
+  };
 });
-
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
-
-var TOKEN_RE = /:[^\/]+/g;
-
-// parseRoute :: String -> RegExp
-var routeRe = exports.routeRe = function routeRe(x) {
-  return new RegExp('^' + x.replace(TOKEN_RE, '([^/]+)') + '$');
-};
-
-// route :: (* -> *), String -> Route -> (String -> _)
-var route = exports.route = function route(f, r) {
-  var re = routeRe(r);
-  return function (path) {
-    var match = re.exec(path);
-    if (!match) return;
-    f.apply(undefined, _toConsumableArray(match.slice(1)));
-  };
-};
-
-var createRouter = exports.createRouter = function createRouter() {
-  var routes = [];
-
-  var add = function add(f, r) {
-    return routes.push(route(f, r));
-  };
-  var dispatch = function dispatch() {
-    return routes.forEach(function (x) {
-      return x(window.location.path);
-    });
-  };
-  var go = function go(path, title) {
-    window.history.pushState(path, undefined, title);
-    dispatch();
-  };
-  var handleEvent = function handleEvent(e) {
-    e.preventDefault();
-    go(e.target.href, e.target.title);
-  };
-
-  window.onpopstate = dispatch;
-  dispatch();
-
-  return { add: add, go: go, handleEvent: handleEvent };
-};
-
-},{}]},{},[1]);
